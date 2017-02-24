ad79a5d94a49df1adf1adbac4c26e506aa49fb5c
David Baxter <itsdavidbaxter@gmail.com>
2017-02-24 11:57:45 -0500

    pull keys from external fields object

---
+++ fields_object.js
@@ -0,0 +1,104 @@
+var fields_object = {
+	fields : [
+		{ 
+			"label": "title", 
+			"index": 0, 
+			"count": 0 
+		},
+		{
+			"label": "people",
+			"index": 1,
+			"count": 0
+		},
+		{
+			"label": "makerDisplay",
+			"index": 2,
+			"count": 0
+		},
+		{
+			"label": "geography",
+			"index": 3,
+			"count": 0
+		},
+		{
+			"label": "geoDisplay",
+			"index": 4,
+			"count": 0
+		},
+		{
+			"label": "displayDate",
+			"index": 5,
+			"count": 0
+		},
+		{
+			"label": "beginDate",
+			"index": 6,
+			"count": 0
+		},
+		{
+			"label": "endDate",
+			"index": 7,
+			"count": 0
+		},
+		{
+			"label": "medium",
+			"index": 8,
+			"count": 0
+		},
+		{
+			"label": "period",
+			"index": 9,
+			"count": 0
+		},
+		{
+			"label": "dimensions",
+			"index": 10,
+			"count": 0
+		},
+		{
+			"label": "invno",
+			"index": 11,
+			"count": 0
+		},
+		{
+			"label": "creditline",
+			"index": 12,
+			"count": 0
+		},
+		{
+			"label": "exhHistory",
+			"index": 13,
+			"count": 0
+		},
+		{
+			"label": "department",
+			"index": 14,
+			"count": 0
+		},
+		{
+			"label": "collections",
+			"index": 15,
+			"count": 0
+		},
+		{
+			"label": "classifications",
+			"index": 16,
+			"count": 0
+		},
+		{
+			"label": "name",
+			"index": 17,
+			"count": 0
+		},
+		{
+			"label": "names",
+			"index": 18,
+			"count": 0
+		},
+		{
+			"label": "publicCaptionEntries",
+			"index": 19,
+			"count": 0
+		}
+	]
+}
